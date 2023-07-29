87570448fac0f023dbcb5064cb695876273e33de
Clement Liaw <cklliaw@uwaterloo.ca>
2023-07-29 13:11:49 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,69 +1,104 @@
-import random
+import re
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-def valid_int_input(user_input):
-  try:
-      user_input = int(user_input)
-  except ValueError:
-      print("That is not a number. Try again.")
-      return False
-
-  return True
-
-def positive_number_input(user_input):
-  if user_input < 0:
-    print("You cannot enter a negative number. Try again.")
-    return False
-  else:
-    return True
-
-print("Welcome to the Password Generator!")
-valid_input = False
-while not valid_input:
-  nr_letters = input("How many letters would you like in your password? ")
-  valid_input = valid_int_input(nr_letters)
-  if valid_input == True:
-    nr_letters = int(nr_letters)
-    valid_input = positive_number_input(nr_letters)
-
-valid_input = False
-while not valid_input:
-  nr_symbols = input(f"How many symbols would you like? ")
-  valid_input = valid_int_input(nr_symbols)
-  if valid_input == True:
-    nr_symbols = int(nr_symbols)
-    valid_input = positive_number_input(nr_symbols)
-
-valid_input = False
-while not valid_input:
-  nr_numbers = input(f"How many numbers would you like? ")
-  valid_input = valid_int_input(nr_numbers)
-  if valid_input == True:
-    nr_numbers = int(nr_numbers)
-    valid_input = positive_number_input(nr_numbers)
-
-password_characters = []
-for char in range(0, nr_letters):
-  password_characters.append(random.choice(letters))
-
-for char in range(0, nr_symbols):
-  password_characters.append(random.choice(symbols))
-
-for char in range(0, nr_numbers):
-  password_characters.append(random.choice(numbers))
-
-password_length = nr_letters + nr_symbols + nr_numbers
-randomized_password = ""
-for x in range(0, password_length):
-  remaining_char_length = password_length - x
-  random_char_position = random.randint(0, remaining_char_length-1)
-  random_char = password_characters[random_char_position]
-  randomized_password = randomized_password + random_char
-  password_characters.pop(random_char_position)
-
-print(f"Here is your random password: {randomized_password}")
+mRNA = input("Enter a mRNA sequence: ").upper()
 
 
+def find_start(sequence) -> int:
+    regex = re.compile("AUG")
+    match = regex.search(sequence)
+    if match:
+        return match.start()
+    else:
+        return -1
+
+
+def find_stop(sequence):
+    regex = re.compile("UAA|UGA|UAG")
+    match = regex.search(sequence)
+    if match:
+        return match.start()
+    else:
+        return -1
+
+
+def rna_to_aa(rna_seq):
+    aa_seq = ["Met"]
+    for p in range(3, len(rna_seq), 3):
+        codon = rna_seq[p : p + 3]
+        if codon == "UUU" or codon == "UUC":
+            aa_seq.append("-Phe")
+        elif (
+            codon == "UUA"
+            or codon == "UUG"
+            or codon == "CUU"
+            or codon == "CUC"
+            or codon == "CUA"
+            or codon == "CUG"
+        ):
+            aa_seq.append("-Leu")
+        elif codon == "AUU" or codon == "AUC" or codon == "AUA":
+            aa_seq.append("-Ile")
+        elif codon == "GUU" or codon == "GUC" or codon == "GUA" == codon == "GUG":
+            aa_seq.append("-Val")
+        elif codon == "UCU" or codon == "UCC" or codon == "UCA" or codon == "UCG":
+            aa_seq.append("-Ser")
+        elif codon == "CCU" or codon == "CCC" or codon == "CCA" or codon == "CCG":
+            aa_seq.append("-Pro")
+        elif codon == "ACU" or codon == "ACC" or codon == "ACA" or codon == "ACG":
+            aa_seq.append("-Thr")
+        elif codon == "GCU" or codon == "GCU" or codon == "GCA" or codon == "GCG":
+            aa_seq.append("-Ala")
+        elif codon == "UAU" or codon == "UAC":
+            aa_seq.append("-Tyr")
+        elif codon == "CAU" or codon == "CAC":
+            aa_seq.append("-His")
+        elif codon == "CAA" or codon == "CAG":
+            aa_seq.append("-Gln")
+        elif codon == "AAU" or codon == "AAC":
+            aa_seq.append("-Asn")
+        elif codon == "AAA" or codon == "AAG":
+            aa_seq.append("-Lys")
+        elif codon == "GAU" or codon == "GAC":
+            aa_seq.append("-Asp")
+        elif codon == "GAA" or codon == "GAG":
+            aa_seq.append("-Glu")
+        elif codon == "UGU" or codon == "UGC":
+            aa_seq.append("-Cys")
+        elif codon == "UGG":
+            aa_seq.append("-Trp")
+        elif (
+            codon == "CGU"
+            or codon == "CGC"
+            or codon == "CGA"
+            or codon == "CGG"
+            or codon == "AGA"
+            or codon == "AGG"
+        ):
+            aa_seq.append("-Arg")
+        elif codon == "AGU" or codon == "AGC":
+            aa_seq.append("-Ser")
+        elif codon == "GGU" or codon == "GGC" or codon == "GGA" or codon == "GGG":
+            aa_seq.append("-Gly")
+        else:
+            print(f"Error: invalid sequence {codon}")
+            exit(1)
+
+    print(f'Your mRNA codes for the amino acid sequence: {"".join(aa_seq)}')
+
+
+def main():
+    start = find_start(mRNA)
+    end = find_stop(mRNA)
+
+    if start < 0:
+        print("Error: could not find start")
+        exit(1)
+    elif end < 0:
+        print("Error: could not find end")
+        exit(1)
+
+    rna_to_aa(mRNA[start:end])
+
+
+if __name__ == "__main__":
+    main()
